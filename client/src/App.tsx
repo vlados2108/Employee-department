1cfbacc0bb170290e3ef0e63180d1c18a1f515fa
vlados2108 <verlov.vladislav@mail.ru>
2023-04-03 23:02:32 +0300

    created tRPC server and client, connected DataBase using prisma. Need to fix: tRPC is not visible in client/src/trpc.ts,when including server as library

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { getFetch, httpBatchLink, loggerLink } from "@trpc/client";
+import { trpc } from "./trpc";
+
+function AppContent() {
+  const hello = trpc.sayHello.useQuery();
+  return <main className="p-2">{JSON.stringify(hello.data, null, 2)}</main>;
+}
 
 function App() {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 5 * 1000,
+          },
+        },
+      })
+  );
+
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        loggerLink(),
+        httpBatchLink({
+          url: "http://localhost:8000/api/trpc",
+          fetch: async (input, init?) => {
+            const fetch = getFetch();
+            return fetch(input, {
+              ...init,
+              credentials: "include",
+            });
+          },
+        }),
+      ],
+    })
+  );
   return (
-    <></>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <AppContent />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </trpc.Provider>
   );
 }
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,46 @@
-import express from "express"
+import path from "path"
+import cors from "cors"
+import dotenv from "dotenv";
+import express from "express";
+import { inferAsyncReturnType, initTRPC } from '@trpc/server';
+import * as trpcExpress from '@trpc/server/adapters/express';
+import z from "zod"
+import { env } from 'process';
+
+dotenv.config({ path: path.join(__dirname, "../env") });
+
+// created for each request
+const createContext = ({
+    req,
+    res,
+}: trpcExpress.CreateExpressContextOptions) => ({ req, res });
+export type Context = inferAsyncReturnType<typeof createContext>;
+
+const t = initTRPC.context<Context>().create();
+const appRouter = t.router({
+    sayHello: t.procedure.query(async () => {
+        const message = "hello"
+        return { message };
+    })
+});
+
+export type AppRouter = typeof appRouter;
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use(
+    cors({
+        origin: "*",
+        credentials: true
+    }));
 
-app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) });
+app.use(
+    "/api/trpc",
+    trpcExpress.createExpressMiddleware({
+        router: appRouter,
+        createContext,
+    })
+);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
