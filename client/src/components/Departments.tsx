15733d1be90ada22d93418f514279899dfb7d52f
vlados2108 <verlov.vladislav@mail.ru>
2023-04-06 23:13:51 +0300

    added departments table

--- client/src/components/Departments.tsx
+++ client/src/components/Departments.tsx
@@ -1,8 +1,23 @@
 import React from "react"
+import { } from "../style/table.css"
+
 function Departments() {
     return (
         <>
-            Hello Departments
+            <div className="flex-container">
+                <div className="wrapper">
+                    <div className="cell">id</div>
+                    <div className="cell">name</div>
+                    <div className="cell">foundation date</div>
+                    <div className="cell">description</div>
+                    <div className="cell">company</div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                    <div className="cell"></div>
+                </div>
+            </div>
         </>
     )
 }
